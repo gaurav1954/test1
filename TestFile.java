1db2ed807d545f9190635b268a4c82a8693b44c7
gaurav <500090993@stu.upes.ac.in>
2023-09-08 07:14:32 +0530

    create new

--- TestFile.java
+++ TestFile.java
@@ -2,6 +2,9 @@ class TestFile{
   public void fun1(){
     System.out.println("hello from main branch");
   }
+  public void fun2(){
+    System.out.println("hello from new branch");
+  }
   public static void main(String args[]){
     TestFile obj=new TestFile();
     obj.fun1();
